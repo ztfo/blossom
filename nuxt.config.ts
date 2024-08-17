f67677ff58747df71d4c78806a6cc69dbb940269
Luis E. Palomares <lepalomares@me.com>
2024-08-17 15:45:20 -0500

    removed symbolic link setup for dist folder and added nojekyll file to build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,27 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { existsSync, writeFileSync } from 'fs'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   target: 'static',
   generate: {
+    dir: 'dist',
     fallback: true 
   },
   build: {
     loaders: {
-      pugPlain: 'pug-plain-loader', 
+      pugPlain: 'pug-plain-loader',
     },
   },
   app: {
-    baseURL: '/', 
+    baseURL: '/blossom/',
   },
+  hooks: {
+    'generate:done': () => {
+      const nojekyllPath = 'dist/.nojekyll'
+      if (!existsSync(nojekyllPath)) {
+        writeFileSync(nojekyllPath, '')
+      }
+    }
+  }
 })
